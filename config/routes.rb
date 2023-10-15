26337a1510831c78dfad3b3f87843023c11e80b6
kimanifinplus <kimani@finplusgroup.com>
2023-10-15 20:04:15 +0300

    Handle update operation for signed in users only

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :members, only: [:show] do
     collection do
-      get "edit_description(/:id)", to: "members#edit_description", as: :edit_description
-      patch "update_description(/:id)", to: "members#update_description", as: :update_description
+      get "edit_description", to: "members#edit_description", as: :edit_description
+      patch "update_description", to: "members#update_description", as: :update_description
     end
   end
 end
