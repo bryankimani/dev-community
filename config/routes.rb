bed441ffee265580c60d43dd3023d425b6ee6be7
kimanifinplus <kimani@finplusgroup.com>
2023-10-15 18:31:14 +0300

    Update user description via turbo streams

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'home#index'
-  resources :members, only: [:show]
+  resources :members, only: [:show] do
+    collection do
+      get "edit_description(/:id)", to: "members#edit_description", as: :edit_description
+      patch "update_description(/:id)", to: "members#update_description", as: :update_description
+    end
+  end
 end
